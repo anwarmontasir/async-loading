88e01e2f64dbf7cf58982f417406fcfca23af469
Anwar Montasir <anwarmontasir@gmail.com>
2018-03-06 12:39:30 -0800

    App

---
+++ src/components/app/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Notes from '../notes/Notes';
+import './App.css';
+
+class App extends Component {
+
+  render() {
+    const { loading, error } = this.props;
+    return (
+      <div className="grid-wrap">
+        <header id="main-header" role="banner">
+          <h1>App name</h1>
+        </header>
+        <main id="main-content" role="main">
+          <p>{loading ? 'I am loading' : 'I am NOT loading'}</p>
+          <Notes />
+          { error && 
+            <pre style={{ color: 'red '}}>
+              {error.message
+                ? error.message
+                : error.error ? error.error : error
+              }
+            </pre>
+          }
+        </main>
+        <footer id="main-footer" role="contentinfo">
+          <small>&copy; 2018 this app.</small>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  state => ({
+    loading: state.loading,
+    error: state.error
+  }),
+  null
+)(App);
